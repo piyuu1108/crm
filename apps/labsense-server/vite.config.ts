0083ee65089c8e665a7f10e0a4497dcf8ccde3b6
Mohit Rajput <mohitrajputt7780@gmail.com>
2026-05-12 23:24:19 +0530

    seted svelte

---
+++ apps/labsense-server/vite.config.ts
@@ -0,0 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [tailwindcss(), sveltekit()],
+  server: {
+    port: 4000, // changed from 3000 to avoid conflict with erp
+  },
+});
